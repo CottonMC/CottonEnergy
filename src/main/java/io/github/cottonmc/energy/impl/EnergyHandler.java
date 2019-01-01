fe9886a2e5db69d3350c7704bf4f1136103cfbe4
Meredith Espinosa <goclonefilms@gmail.com>
2018-12-31 18:34:20 -0800

    actually implement listeners

--- src/main/java/io/github/cottonmc/energy/impl/EnergyHandler.java
+++ src/main/java/io/github/cottonmc/energy/impl/EnergyHandler.java
@@ -1,15 +1,18 @@
 package io.github.cottonmc.energy.impl;
 
+import com.google.common.collect.Lists;
 import io.github.cottonmc.energy.api.ActionType;
 import io.github.cottonmc.energy.api.EnergyComponent;
 import io.github.cottonmc.energy.api.Observable;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 
 public class EnergyHandler implements EnergyComponent, Observable {
 
 	private int maxEnergy;
 	private int currentEnergy = 0;
+	private final List<Runnable> listeners = Lists.newArrayList();
 
 	public EnergyHandler(int maxEnergy) {
 		this.maxEnergy = maxEnergy;
@@ -41,6 +44,7 @@ public class EnergyHandler implements EnergyComponent, Observable {
 		int insertRoom = maxEnergy - currentEnergy;
 		int insertAmount = (amount <= insertRoom)? amount : insertRoom;
 		if (actionType == ActionType.PERFORM) currentEnergy += insertAmount;
+		onChanged();
 		return insertAmount;
 	}
 
@@ -49,21 +53,28 @@ public class EnergyHandler implements EnergyComponent, Observable {
 	public int extractEnergy(int amount, ActionType actionType) {
 		int extractAmount = (amount <= currentEnergy)? amount : currentEnergy;
 		if (actionType == ActionType.PERFORM) currentEnergy -= extractAmount;
+		onChanged();
 		return extractAmount;
 	}
 
 	void setCurrentEnergy(int amount) {
 		if (amount <= maxEnergy) currentEnergy = amount;
 		else currentEnergy = maxEnergy;
+		onChanged();
 	}
 
 	void setMaxEnergy(int amount) {
 		maxEnergy = amount;
 		if (currentEnergy > maxEnergy) currentEnergy = maxEnergy;
+		onChanged();
 	}
 
 	@Override
 	public void listen(@Nonnull Runnable r) {
-		r.run();
+		listeners.add(r);
+	}
+
+	public void onChanged() {
+		listeners.forEach(Runnable::run);
 	}
 }
