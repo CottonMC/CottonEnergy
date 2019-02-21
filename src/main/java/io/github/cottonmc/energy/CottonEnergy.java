0f3e22f06fbd0e22578f6996495618bf43d90600
Meredith Espinosa <goclonefilms@gmail.com>
2019-02-21 00:26:04 -0800

    update to 08a, remove unneeded serializer

--- src/main/java/io/github/cottonmc/energy/CottonEnergy.java
+++ src/main/java/io/github/cottonmc/energy/CottonEnergy.java
@@ -1,6 +1,8 @@
 package io.github.cottonmc.energy;
 
 
+import net.minecraft.util.registry.DefaultedRegistry;
+import net.minecraft.util.registry.MutableRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,16 +10,14 @@ import io.github.cottonmc.energy.api.EnergyType;
 import io.github.cottonmc.energy.api.DefaultEnergyTypes;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.DefaultMappedRegistry;
-import net.minecraft.util.registry.ModifiableRegistry;
 import net.minecraft.util.registry.Registry;
 
 public class CottonEnergy implements ModInitializer {
     private static final Logger LOG = LogManager.getLogger("Cotton-Energy");
     public static final Registry<EnergyType> ENERGY_REGISTRY;
     static { //Be ready before our admirers!
-        ModifiableRegistry<EnergyType> temp_init = new DefaultMappedRegistry<EnergyType>("cotton:low_voltage");
-        Registry.REGISTRIES.register( new Identifier("cotton", "energy"), temp_init );
+        MutableRegistry<EnergyType> temp_init = new DefaultedRegistry<>("cotton:low_voltage");
+        Registry.register(Registry.REGISTRIES, new Identifier("cotton", "energy"), temp_init );
         ENERGY_REGISTRY = temp_init;
 
         DefaultEnergyTypes.init();
