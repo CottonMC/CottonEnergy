334a74d047dfd3bec3639fe03f77f558f105e9b1
Pannoniae <prisznyak.zsombor@gmail.com>
2019-02-26 21:50:04 +0000

    - format WUs

--- src/main/java/io/github/cottonmc/energy/api/ElectricalEnergyType.java
+++ src/main/java/io/github/cottonmc/energy/api/ElectricalEnergyType.java
@@ -19,6 +19,20 @@ public class ElectricalEnergyType implements EnergyType {
     @Override
     public TextComponent getDisplayAmount(int amount) {
         //TODO: handle KWU, MWU, and GWU
-        return new TranslatableTextComponent("info.cotton.energy.electrical.amount", amount);
+        if (amount < 1000) { // x < 1M
+            return new TranslatableTextComponent("info.cotton.energy.electrical.amount", amount);
+        }
+        else if (amount < 1_000_000) { // 1K < x < 1M
+            float tAmount = amount / 1000;
+            return new TranslatableTextComponent("info.cotton.energy.electrical.amount.k", tAmount);
+        }
+        else if (amount < 1_000_000_000) { // 1M < x < 1G
+            float tAmount = amount / 1_000_1000;
+            return new TranslatableTextComponent("info.cotton.energy.electrical.amount.m", tAmount);
+        }
+        else { // 1G < x
+            float tAmount = amount / 1_000_000_000;
+            return new TranslatableTextComponent("info.cotton.energy.electrical.amount.g", tAmount);
+        }
     }
 }
--- src/main/java/io/github/cottonmc/energy/api/EnergyType.java
+++ src/main/java/io/github/cottonmc/energy/api/EnergyType.java
@@ -5,8 +5,10 @@ import net.minecraft.text.TextComponent;
 public interface EnergyType {
     /** @return the minimum amount of energy that can be transferred at a time. */
 	default int getMinimumTransferSize() { return 0; }
+
 	/** @return the maximum amount of energy that can be transferred at a time. */
 	int getMaximumTransferSize();
+
 	/**
 	 * Gets human-readable text representing an energy value of this type.
 	 * @param amount the total aggregate number of energy units being represented.
