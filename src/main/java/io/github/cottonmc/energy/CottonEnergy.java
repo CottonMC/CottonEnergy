188330a0a9f2ee22df78f3914a8d6ad1c23807cd
Pannoniae <prisznyak.zsombor@gmail.com>
2019-02-26 23:02:22 +0000

    - fix build

--- src/main/java/io/github/cottonmc/energy/CottonEnergy.java
+++ src/main/java/io/github/cottonmc/energy/CottonEnergy.java
@@ -1,10 +1,11 @@
 package io.github.cottonmc.energy;
 
 
+import io.github.cottonmc.cotton.Cotton;
+import io.github.cottonmc.cotton.logging.Ansi;
+import io.github.cottonmc.cotton.logging.ModLogger;
 import net.minecraft.util.registry.DefaultedRegistry;
 import net.minecraft.util.registry.MutableRegistry;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import io.github.cottonmc.energy.api.EnergyType;
 import io.github.cottonmc.energy.api.DefaultEnergyTypes;
@@ -13,11 +14,11 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class CottonEnergy implements ModInitializer {
-    private static final Logger LOG = Cotton.logger;
+    private static final ModLogger logger = new ModLogger("cotton-energy", "COTTON ENERGY");
     public static final Registry<EnergyType> ENERGY_REGISTRY;
     static { //Be ready before our admirers!
         MutableRegistry<EnergyType> temp_init = new DefaultedRegistry<>("cotton:low_voltage");
-        Registry.register(Registry.REGISTRIES, new Identifier("cotton", "energy"), temp_init );
+        Registry.register(Registry.REGISTRIES, new Identifier("cotton", "energy"), temp_init);
         ENERGY_REGISTRY = temp_init;
 
         DefaultEnergyTypes.init();
@@ -25,7 +26,9 @@ public class CottonEnergy implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing cotton-energy!");
+        logger.setPrefixFormat(Ansi.Red);
+
+        logger.info("cotton-energy initialized.");
     }
 
 }
