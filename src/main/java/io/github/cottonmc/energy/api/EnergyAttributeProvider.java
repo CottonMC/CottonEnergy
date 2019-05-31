5d878fe02f56fda0356dd65e3a164f9564c08b68
falseresync <falseresync@gmail.com>
2019-05-31 23:17:32 +0300

    Suitability for items

--- src/main/java/io/github/cottonmc/energy/api/EnergyAttributeProvider.java
+++ src/main/java/io/github/cottonmc/energy/api/EnergyAttributeProvider.java
@@ -1,5 +1,9 @@
 package io.github.cottonmc.energy.api;
 
+import net.minecraft.nbt.CompoundTag;
+
 public interface EnergyAttributeProvider {
-    EnergyAttribute getEnergy();
+    EnergyAttribute getEnergy(CompoundTag tag);
+    EnergyAttribute fromTag(CompoundTag tag);
+    void toTag(EnergyAttribute attribute, CompoundTag tag);
 }
