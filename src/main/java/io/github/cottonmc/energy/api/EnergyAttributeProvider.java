ddbe99ac05c5da73c879e76fe71ada007afd0549
falseresync <falseresync@gmail.com>
2019-06-01 22:54:50 +0300

    Javadoc fix 2

--- src/main/java/io/github/cottonmc/energy/api/EnergyAttributeProvider.java
+++ src/main/java/io/github/cottonmc/energy/api/EnergyAttributeProvider.java
@@ -1,8 +1,8 @@
 package io.github.cottonmc.energy.api;
 
 /**
- * This should NEVER ever be used on ANYTHING that's NOT
- * an ENTITY or WORLD.
+ * This interface MUST NOT be implemented on any subclass
+ * of Block or BlockEntity.
  *
  * For BlockEntities use LibBlockAttributes' existing
  * capabilities. Blocks should never have the attribute.
